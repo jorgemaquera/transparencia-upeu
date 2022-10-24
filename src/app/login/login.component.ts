068fc5daf2bed315163a13ccba6de77e2993a2b5
Jorge Maquera <jjorgemcyt@gmail.com>
2022-10-24 12:42:35 -0500

    VS Worspace settings were added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
