172019be4664393371d023df247a8773ba32b37c
sembh1998 <sembenavente@gmail.com>
2022-10-23 20:26:50 -0500

    avance temporal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DocumentsComponent } from './documents/documents.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSliderModule,
     BrowserAnimationsModule
   ],
   providers: [],
